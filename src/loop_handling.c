c97e8d445b645b03d40b4f1a99adbebdc173cf72
Liam Wynn <wynnliam@pdx.edu>
2018-05-13 12:14:14 -0700

    Added look-up tables for angles in params.h

--- src/loop_handling.c
+++ src/loop_handling.c
@@ -8,12 +8,6 @@
 #include <stdio.h>
 #include <math.h>
 
-// The player's position
-int player_x = 640;
-int player_y = 320;
-// Player rotation in degrees.
-float player_rot = 3.14f;
-
 /*INITIALIZATION PROCEDURES*/
 
 void initialize() {
@@ -24,31 +18,12 @@ void initialize() {
 void update() {
 }
 
-int is_wall(int x, int y) {
-	int unit_x = x / UNIT_SIZE;	
-	int unit_y = y / UNIT_SIZE;
-
-	if(unit_x < 0) unit_x = 0;
-	if(unit_x >= MAP_W) unit_x = MAP_W - 1;
-	if(unit_y < 0) unit_y = 0;
-	if(unit_y >= MAP_H) unit_y = MAP_H - 1;
-
-	return map[unit_y * MAP_W + unit_x] > 0;
-}
-
-void trace(float angle, int hit[2]) {
-}
-
 /*RENDERING PROCEDURES*/
 void render(SDL_Renderer* renderer) {
 	SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
 	// Fills the screen with the current render draw color, which is
 	// cornflower blue.
 	SDL_RenderClear(renderer);
-
-	int i;
-	for(i = 0; i < PROJ_W; ++i) {
-	}
 
 	// Forces the screen to be updated.
 	SDL_RenderPresent(renderer);
--- src/params.h
+++ src/params.h
@@ -7,13 +7,16 @@
 
 // Field of view, in radians. This specifies the angle of what we can
 // view. It is also important for computing the rays themselves.
-#define FOV					1.05
+#define FOV					60
 // This value occurs in the raycaster enough that we ought to pre-compute
 // it.
-#define FOV_HALF			0.525
+#define FOV_HALF			30
 // The world is essentially a series of cubes. This value is the size of
 // each cube or unit as I call it in pixels.
 #define UNIT_SIZE			64
+// Essentially, UNIT_SIZE = 2 ^ UNIT_POWER. We use this to do effecient
+// multiplication and division.
+#define UNIT_POWER			6
 // The size of the projection area, which is our screen. I would call it
 // "SCREEN_W and SCREEN_H", but the underlying math refers to them as
 // the PROJECTION PLANE, so I will use that nomenclature instead.
@@ -23,10 +26,15 @@
 // with some basic trigonometry, but we will have it pre-computed here
 #define DIST_TO_PROJ		277
 // Essentially the FOV / PROJ_W, but we will pre-compute that here.
-#define ANGLE_BETWEEN_RAYS	0.03
+#define ANGLE_BETWEEN_RAYS	0.1875
 
-// Holds the angle of each ray (there are projection plane width number of them).
-// The value stored here is the angle if the player has a rotation of 0. This way,
-// we need only add the player's rotation to these values. They are computed in
-// the initialize function.
-float ray_angles[PROJ_W];
+// Stores the sin value of every degree from 0 to 360 multiplied by 128.
+// This will enable us to preserve enough precision for each number as
+// a byte. When we want a value, we can access sin128table[i] >> 7, which
+// undoes the multiplication.
+char sin128table[361];
+// Stores the cos value of every degree from 0 to 360 multiplied by 128.
+char cos128table[361];
+// Stores every tan value of every degree from 0 to 360 multiplied by 128.
+// The values for 0, 90, 180, 270, and 360 will be -1.
+char tan128Table[361];
