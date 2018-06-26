59727bf33777d246b12bd84583435c9685e15650
Liam Wynn <wynnliam@pdx.edu>
2018-06-26 12:19:16 -0700

    Added animation.h

---
+++ src/animation.h
@@ -0,0 +1,23 @@
+// Liam Wynn, 5/12/2018, CS410p: Full Stack Web Development
+
+/*
+	Defines data structures and procedures relating to
+	animations.
+
+	Let's discuss the nature of animations a little bit here.
+	We'll begin by defining a couple of things. We say that
+	an animation is a series of frames, and each frame is a 64 x 64
+	pixel square component of an overall sprite sheet. So in
+	concrete terms, when we render an animation for a sprite, we
+	render a square component of a larget image.
+
+	There are a couple of requirements for animations for our system here.
+	First, as I stated previously, all animation frames must be 64 by 64 pixels.
+	Second, an animation goes from left to right. This means that the first frame
+	of an animation sequence is the left most 64 by 64 pixel square. The rightmost
+	one is its last.
+*/
+
+struct animdef {
+	// TODO: Finish me!
+};
